7d996798794eb57fea1b01b6a096a1c40a6ef64c
Yves <yves.rouille@gmail.com>
2019-03-16 00:48:32 +0100

    Updates

---
+++ precache-manifest.c87dfa9262744c59af291e6cd34ce316.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbd9934eccede7aaf512",
+    "url": "/datavision/static/js/runtime~main.b62838cd.js"
+  },
+  {
+    "revision": "5e80e4ab70bfad7a8999",
+    "url": "/datavision/static/js/main.45d96d8a.chunk.js"
+  },
+  {
+    "revision": "82379a219e2892ae009c",
+    "url": "/datavision/static/js/2.2d896ddf.chunk.js"
+  },
+  {
+    "revision": "5e80e4ab70bfad7a8999",
+    "url": "/datavision/static/css/main.7eecf5ac.chunk.css"
+  },
+  {
+    "revision": "cda6fb2ed022cbe7a8ad60d25ba326d4",
+    "url": "/datavision/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1158d89789125c46443ae4ced5a1c2d5.js"
+  "/datavision/precache-manifest.c87dfa9262744c59af291e6cd34ce316.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/datavision/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
